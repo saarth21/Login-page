1c9428e18c460106f5a690bc186be001a4118dfa
saarth21 <joshisaarth@gmail.com>
2025-03-17 17:35:09 -0400

    Login page

--- Images.py
+++ Images.py
@@ -5,11 +5,11 @@ window = tk.Tk()
 window.geometry("600x800")
 
 #Display Image
-image = Image.open("Image1.jpg")
-image =image.resize((250,228))
-image = ImageTk.PhotoImage(image)
-image_label = tk.Label(window, image=image)
-image_label.pack(pady=15)
+image = Image.open("Image1.jpg") #Loading the image
+image =image.resize((250,228))   #Resize the image
+image = ImageTk.PhotoImage(image)	# Make into a photoimage object
+image_label = tk.Label(window, image=image)	# Put image onto a label 
+image_label.pack(pady=15)   # Pack the image
 
 
 
