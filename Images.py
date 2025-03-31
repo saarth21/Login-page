2947e3616a6a6bbad43c20cdb243351e802e9b2e
saarth21 <joshisaarth@gmail.com>
2025-03-31 18:29:38 -0400

    Update Images.py

--- Images.py
+++ Images.py
@@ -9,20 +9,20 @@ image = Image.open("Image1.jpg") #Loading the image
 image = image.resize((150,140))   #Resize the image
 image = ImageTk.PhotoImage(image)	# Make into a photoimage object
 gallery_image = tk.Label(window, image=image)	# Put image onto a label 
-gallery_image.pack(pady=15,side="left")   # Pack the image
+gallery_image.grid(row=1, column=0)   # Pack the image
 
 gallery_label = tk.Label(window, text= "Photo gallery")
-gallery_label.pack(pady=5,side="left")
+gallery_label.grid(row=1,column=0)
 
 #Discord app
 image1 = Image.open("discord.jpg")
 image1 = image1.resize((150,140))
 image1 = ImageTk.PhotoImage(image1)
 discord_image = tk.Label(window,image=image1)
-discord_image.pack(pady=15)
+discord_image.grid(row=0, column=0)
 
 discord_label = tk.Label(window, text= "Discord")
-discord_label.pack(pady=5)
+discord_label.grid(row=1, column=0)
 
 
 
